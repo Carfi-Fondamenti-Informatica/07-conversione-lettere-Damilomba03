76f02955a7b5296d5d9d80448d20c8fc86508fce
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-12-07 11:20:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,34 +1,23 @@
-//
-// Created by Damiano on 05/12/2022.
-//
-
 #include "lib.h"
-bool verifica(char a)
-{
+bool verifica(char a){
     int x;
     x=(int)a;
-   if(x>=65 && x<=90 || x>=97 && x<=122)
-   {
-       return true;
-   }
-   else
-   {
-       return false;
-   }
+    if(x>=65 && x<=90 || x>=97 && x<=122){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
-char lettera(char a)
-{
+char lettera(char a){
     int x;
     x=(int)a;
-    int z=0;
-    if(x>=65 && x<=90)
-    {
-        z=x+32;
+    int y=0;
+    if(x>=65 && x<=90){
+        y=x+32;
     }
-    else if(x>=97 && x<=122)
-    {
-        z=x-32;
+    else if(x>=97 && x<=122){
+        y=x-32;
     }
-    return z;
-
+    return y;
 }
