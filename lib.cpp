ead21afdf3ffc411c9314b558075ec1130a9c682
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-12-07 07:32:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,30 @@
+#include "lib.h"
+bool verifica(char a)
+{
+    int x;
+    x=(int)a;
+   if(x>=65 && x<=90 || x>=97 && x<=122)
+   {
+       return true;
+   }
+   else
+   {
+       return false;
+   }
+}
+char lettera(char a)
+{
+    int x;
+    x=(int)a;
+    int z=0;
+    if(x>=65 && x<=90)
+    {
+        z=x+32;
+    }
+    else if(x>=97 && x<=122)
+    {
+        z=x-32;
+    }
+    return z;
 
+}
