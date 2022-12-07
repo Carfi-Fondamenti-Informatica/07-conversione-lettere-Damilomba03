4f939566fa2b72ae7439b02c0fb32db13efcad2e
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-12-07 11:20:54 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,7 +1,6 @@
-#ifndef GITHUB_7_LIB_H
-#define GITHUB_7_LIB_H
-
+#ifndef ESE_7_GITHUB_LIB_H
+#define ESE_7_GITHUB_LIB_H
 bool verifica(char a);
 char lettera(char a);
-#endif //GITHUB_7_LIB_H
+#endif //ESE_7_GITHUB_LIB_H
 
