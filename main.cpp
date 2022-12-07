b3a5b0f1f066690cde0489ae9f3ab37427df8907
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-12-07 07:34:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    char a;
+    cin>>a;
+    if (verifica(a)==true)
+    {
+        cout<<lettera(a)<<endl;
+    }
+    else
+    {
+        cout<<"ERRORE"<<endl;
+    }
+    return 0;
 }
